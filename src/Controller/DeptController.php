5d0c7c324e14a9fe4c6c9241402e8d462f7ee0b1
Zal-com <66016928+Zal-com@users.noreply.github.com>
2022-12-17 13:19:19 +0100

    Nouveaux fichiers

--- src/Controller/DeptController.php
+++ src/Controller/DeptController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Department;
 use App\Repository\DepartmentRepository;
+use App\Repository\EmployeeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/departements')]
 class DeptController extends AbstractController
 {
-    #[Route('/departements', name: 'app_dept')]
-    public function index(DepartmentRepository $depts): Response
+    #[Route('/', name: 'app_dept_index')]
+    public function index(DepartmentRepository $depts, EmployeeRepository $employees): Response
     {
         return $this->render('dept/index.html.twig', [
             'departements' => $depts->findAll(),
+        ]);
+    }
+    #[Route('/show/{id}', name:'app_dept_show', methods: ['GET'])]
+    public function show(Department $department):Response{
+        return $this->render('dept/show.html.twig',[
+            'departement' => $department,
         ]);
     }
 }
